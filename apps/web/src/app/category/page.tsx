388477a2a676244763ee619de30703c571133ff9
seventhse <season.sevent@icloud.com>
2024-09-30 10:45:37 +0800

    test: Category local data

--- apps/web/src/app/category/page.tsx
+++ apps/web/src/app/category/page.tsx
@@ -1,23 +1,5 @@
-import { getArticleCategory, getArticlesByCategory } from '@/api/article.ts'
-import { ArticleList } from '@/components/article-list.tsx'
-import { CategoryTab } from '@/components/category-tab.tsx'
-
-export async function generateStaticParams() {
-  return await getArticleCategory()
-}
+import { redirect } from 'next/navigation'
 
 export default async function Page() {
-  const categoryes = await getArticleCategory()
-  const articles = await getArticlesByCategory('all')
-
-  return (
-    <div className="container-px">
-      <header className="my-3">
-        <CategoryTab slug="all" categoryes={categoryes} />
-      </header>
-      <main className="size-full mx-auto flex flex-col gap-y-3">
-        <ArticleList articles={articles} />
-      </main>
-    </div>
-  )
+  redirect('/category/all')
 }
